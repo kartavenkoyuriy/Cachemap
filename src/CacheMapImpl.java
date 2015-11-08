7922572027a05af71f07468fbda9937426f5b663
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2015-11-09 01:46:50 +0200

    pass all test, but not optimized(linked list realization)

--- src/CacheMapImpl.java
+++ src/CacheMapImpl.java
@@ -1,43 +1,26 @@
 public class CacheMapImpl<K, V> implements CacheMap {
 
     private long timeToLive = 0;
-    private long defaultTimeToLive = 60000;
-    private Entry<K, V>[] entries;//linkedlist?
+    private Entry head;
+    private Entry tail;
 
-    static class Entry<K, V> {//linkedlist?
-        K key;
-        V value;
-        long currentEntryDate;
+    static class Entry<K, V> {
+        private K key;
+        private V value;
+        private Entry<K, V> prev;
+        private Entry<K, V> next;
+        private long currentEntryDate;
 
-        public Entry(K key, V value, long currentEntryDate) {
+        //@param key may not be null //in tests
+
+        public Entry(K key, V value, Entry<K, V> prev, Entry<K, V> next, long currentEntryDate) {
             this.key = key;
             this.value = value;
+            this.prev = prev;
+            this.next = next;
             this.currentEntryDate = currentEntryDate;
         }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public long getCurrentEntryDate() {
-            return currentEntryDate;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            Entry<K, V> newEntry = (Entry<K, V>) obj;
-            return (this.key.equals(newEntry.key) && this.value.equals(newEntry.value));
-        }
     }
-
 
     @Override
     public void setTimeToLive(long timeToLive) {
@@ -46,140 +29,150 @@ public class CacheMapImpl<K, V> implements CacheMap {
 
     @Override
     public long getTimeToLive() {
+        long defaultTimeToLive = 60000;
         return timeToLive == 0 ? defaultTimeToLive : timeToLive;
     }
 
     @Override
     public V put(Object key, Object value) {
-//        clearExpired();
-        Entry entry = new Entry(key, value, Clock.getTime());
-        if (entries == null) {
-            entries = new Entry[4];
-            entries[0] = entry;
+        clearExpired();
+        if (head == null) {
+            head = tail = new Entry(key, value, null, null, Clock.getTime());
+            return null;
         } else {
-            if(entries[entries.length - 1] != null){
-                Entry<V,K>[] oldEntries = (Entry<V, K>[]) entries;//Arrays.copy
-                entries = new Entry[entries.length * 2];
-                for (int j = 0; j < entries.length; j++) {
-                    if (oldEntries[j] != null){
-                        entries[j] = (Entry<K, V>) oldEntries[j];
-                    } else {
-                        entries[j] = null;//continue
-                    }
-                }
-            }
-            for (int i = 0; i < entries.length; i++) {
-                if(entries[i] == null){
-                    entries[i] = entry;
-                    return (i - 1 < 0) ? null : entries[i - 1].getValue();
-                }
-            }
+            Entry temp = new Entry(key, value, tail, null, Clock.getTime());
+            tail.next = temp;
+            tail = temp;
+            return (V) tail.prev;
         }
-        return null;
     }
 
     @Override
     public void clearExpired() {
-        if(entries == null){
+        if (head == null) {
             return;
         }
-        for (int i = 0; i < entries.length; i++) {
-            if(entries[i] == null){
-                continue;
+
+        Entry temp = head;
+        while (temp != null) {
+            if (Clock.getTime() - temp.currentEntryDate > getTimeToLive()) {
+                if (head == tail) {
+                    head = null;
+                } else {
+                    if(temp == head){
+                        head = head.next;
+                    }
+                    if (temp == tail){
+                        tail = tail.prev;
+                    }
+                    if (temp.prev != null){
+                        temp.prev.next = temp.next;
+                    }
+                    if (temp.next != null){
+                        temp.next.prev = temp.prev;
+                    }
+                }
             }
-            System.out.println("entries[i].getCurrentEntryDate():" + entries[i].getCurrentEntryDate());
-            System.out.println("getTimeToLive():" + getTimeToLive());
-            System.out.println("entries[i].getCurrentEntryDate() - currentTime > getTimeToLive():" + (entries[i].getCurrentEntryDate() < getTimeToLive()));
-            if(entries[i].getCurrentEntryDate() < getTimeToLive()){
-                entries[i] = null;
-            }
+            temp = temp.next;
         }
     }
 
     @Override
     public void clear() {
-        for (int i = 0; i < entries.length; i++) {
-            entries[i] = null;
-        }
+        head = tail = null;
     }
 
     @Override
     public boolean containsKey(Object key) {
-//        clearExpired();
-        for (int i = 0; i < entries.length; i++) {
-            if(entries[i].getKey().equals(key)){
+        clearExpired();
+
+        Entry temp = head;
+        while (temp != null) {
+            if (temp.key.equals(key)) {
                 return true;
             }
+            temp = temp.next;
         }
+
         return false;
     }
 
     @Override
     public boolean containsValue(Object value) {
-//        clearExpired();
-        for (int i = 0; i < entries.length; i++) {
-            if(entries[i].getValue().equals(value)){
+        clearExpired();
+
+        Entry temp = head;
+        while (temp != null) {
+            if (temp.value.equals(value)) {
                 return true;
             }
+            temp = temp.next;
         }
+
         return false;
     }
 
     @Override
     public Object get(Object key) {
-        //clearExpired();
-        for (int i = 0; i < entries.length; i++) {
-            if(entries[i] == null){
-                return null;
+        clearExpired();
+
+        Entry temp = head;
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                return temp.value;
             }
-            if(entries[i].getKey().equals(key)){
-                return entries[i].getValue();
-            }
+            temp = temp.next;
         }
+
         return null;
     }
 
     @Override
     public boolean isEmpty() {
-//        clearExpired();
-        for (int i = 0; i < entries.length; i++) {
-            if(entries[i] != null){
-                return false;
-            }
-        }
-        return true;
+        clearExpired();
+
+        return head == null;
     }
 
     @Override
     public Object remove(Object key) {
-        for (int i = 0; i < entries.length; i++) {
-            if(entries[i].getKey().equals(key)){
-                entries[i] = null;
-                return (i - 1 < 0) ? null : entries[i - 1].getValue();
+        clearExpired();
+
+        Entry temp = head;
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                V oldValue = (V) temp.value;
+                if(temp == head){
+                    head = head.next;
+                }
+                if (temp == tail){
+                    tail = tail.prev;
+                }
+                if (temp.prev != null){
+                    temp.prev.next = temp.next;
+                }
+                if (temp.next != null){
+                    temp.next.prev = temp.prev;
+                }
+                return oldValue;
             }
+            temp = temp.next;
         }
+
         return null;
     }
 
     @Override
     public int size() {
         clearExpired();
-        int size = 0;
-        for (int i = 0; i < entries.length; i++) {
-            if(entries[i] != null){
-                size++;
-            }
+
+        int count = 0;
+        Entry temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
         }
-        return size;
-    }
 
-    public static void main(String[] args) {
-        CacheMapImpl cacheMap = new CacheMapImpl();
-        cacheMap.put(1, "asd");
-        System.out.println(cacheMap.size());
-        System.out.println(cacheMap.get(1));
-        cacheMap.setTimeToLive(1);
-        System.out.println(cacheMap.size());
+        return count;
     }
-
 }
